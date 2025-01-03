49719e91bfa6cf87db0b1d76329d8ebd5867d5eb
Selin <sbudak@cyh.com.tr>
2025-01-03 10:42:02 +0300

    created movie class

---
+++ application/Movie.java
@@ -0,0 +1,69 @@
+
+
+package application;
+
+public class Movie {
+	private int movieId;
+	private String movieTitle;
+	private String movieGenre;
+	private String posterUrl;
+	private String summaryFilePath;
+	
+
+	Movie(int movieId,String movieTitle,String movieGenre,String posterUrl,String summaryFilePath){
+		this.movieId=movieId;
+		this.movieTitle=movieTitle;
+		this.movieGenre=movieGenre;
+		this.posterUrl=posterUrl;
+		this.summaryFilePath=summaryFilePath;
+		
+	}
+	
+	
+	
+
+	public int getMovieId() {
+		return this.movieId;
+		
+	}
+	
+	
+	public String getMovieTitle() {
+		return this.movieTitle;
+		
+	}
+	
+	public String getMovieGenre() {
+		return this.movieGenre;
+		
+	}
+	
+	public String getposterUrl() {
+		return this.posterUrl;
+		
+	}
+	
+	public String getSummaryFilePath() {
+		return this.summaryFilePath;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
